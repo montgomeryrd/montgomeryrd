fba2254656e88ebb1c399bcc56ff21df0026097c
montgomeryrd <montgomery_rd@yahoo.com>
2020-12-29 22:06:11 -0800

    finished banner links

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import MenuBurger from '../MenuBurger/MenuBurger';
+import ContactLinks from '../ContactLinks/ContactLinks';
+
+const About = () => {
+    return (
+        <div>
+            <MenuBurger />
+            <ContactLinks />
+        </div>
+    )
+}
+
+export default About;
